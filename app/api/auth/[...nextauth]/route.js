f7d7d7964bffe275db8a867b3b5050ab1f3b5f40
ourhouchmohamed97 <ourhouchmohamed97@gmail.com>
2025-02-05 21:48:28 +0100

    push v1 Propmptopia

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google';
+
+import { connectToDB } from '@utils/database';
+
+console.log({
+	clientId: process.env.GOOGLE_ID,
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+})
+
+const handler = NextAuth({
+	providers:[
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		})
+	],
+	async session({ session }) {
+
+	},
+	async signIn({ profile }) {
+		try {
+			// serverLess -> lambda -> dynamodb
+			await connectToDB();
+
+			// check if a user already exists
+
+			// if not, create a new user
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	}
+})
+
+export { handler as GET, handler as POST };
